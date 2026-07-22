c63061314eceac35021e7b89240e4bf771ab2fe0
Harsh <harsh1111kashyap@gmail.com>
2026-07-23 00:37:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 
 
-let buttons = document.querySelectorAll(".number-key");
-
-buttons.forEach((btn)=>{
-    btn.addEventListener("click", (e) => {
-        
-;    })
-})
 
 
 let menuBtn = document.getElementById("menu-but");
 let menuScreen = document.getElementById("menu-screen");
 menuBtn.addEventListener("click", () => {
     menuScreen.classList.toggle("menu-show")
+})
+
+
+let buttons = document.querySelectorAll(".number-key");
+
+let input = document.getElementById("input-screen");
+buttons.forEach((btn)=>{
+    btn.addEventListener("click", (e) => {
+        input.value += btn.innerHTML;
+;    })
 })
